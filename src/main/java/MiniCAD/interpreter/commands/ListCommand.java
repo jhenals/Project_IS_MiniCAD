09da1820a6b39ce5bcdf1be7078f137f002a5e8a
Jhenals <suboljhenalyn3@gmail.com>
2024-07-10 11:39:17 +0200

    update list command class

--- src/main/java/MiniCAD/interpreter/commands/ListCommand.java
+++ src/main/java/MiniCAD/interpreter/commands/ListCommand.java
@@ -32,46 +32,70 @@ public class ListCommand implements Command{
 
         switch( parametro.getTipo() ){
             case OBJ_ID -> {
-                GraphicObject object = objectManager.getObject(parametro.getValore().toString());
-                System.out.println("Oggetto con id " + parametro.getValore().toString());
-                System.out.println("Tipo:" + object.getType());
-                System.out.println("Dimensione" + object.getDimension());
-                System.out.println("Posizione corrente" + object.getPosition());
+                if (objectManager.getObject(parametro.getValore().toString()) == null) {
+                    System.out.println("Oggetto non esiste");
+                } else {
+                    GraphicObject object = objectManager.getObject(parametro.getValore().toString());
+                    System.out.println("Elenco di proprietà dell'oggetto con id=" + parametro.getValore().toString());
+                    System.out.println(" Tipo: " + object.getType());
+                    System.out.println(" Dimensione: " + object.getDimension());
+                    System.out.println(" Posizione corrente: " + stampaPosizione(object));
+                }
             }
             case CIRCLE -> {
                 List<GraphicObject> circles= objectManager.getObjectsByType("Circle");
                 StringBuilder sb= new StringBuilder();
-                sb.append("Oggetti di tipo CERCHIO:\n");
-                for( GraphicObject go : circles ){
-                    sb.append(objectManager.getKey(go) + " in posizione:" + go.getPosition().toString());
+                if(circles.isEmpty()){
+                    sb.append(" VUOTO");
+                }else {
+                    sb.append("Oggetti di tipo CERCHIO:\n");
+                    for (GraphicObject go : circles) {
+                        sb.append(" " + objectManager.getKeyByValue(go) + " in posizione:" + stampaPosizione(go) + "\n");
+                    }
                 }
+                System.out.println(sb.toString());
             }
 
             case RECTANGLE -> {
                 List<GraphicObject> rectangles= objectManager.getObjectsByType("Rectangle");
                 StringBuilder sb= new StringBuilder();
                 sb.append("Oggetti di tipo RETTANGOLO:\n");
-                for( GraphicObject go : rectangles ){
-                    sb.append(objectManager.getKey(go) + " in posizione:" + go.getPosition().toString());
+                if(rectangles.isEmpty()){
+                    sb.append(" VUOTO");
+                }else {
+                    for( GraphicObject go : rectangles ){
+                        sb.append(" " + objectManager.getKeyByValue(go) + " in posizione:" + stampaPosizione(go) +"\n");
+                    }
                 }
+                System.out.println(sb.toString());
             }
 
             case IMG -> {
                 List<GraphicObject> images= objectManager.getObjectsByType("Image");
                 StringBuilder sb= new StringBuilder();
                 sb.append("Oggetti di tipo IMMAGINE:\n");
-                for( GraphicObject go : images ){
-                    sb.append(objectManager.getKey(go) + " in posizione:" + go.getPosition().toString());
+                if(images.isEmpty()){
+                    sb.append(" VUOTO");
+                }else {
+                    for (GraphicObject go : images) {
+                        sb.append(" " + objectManager.getKeyByValue(go) + " in posizione:" + stampaPosizione(go) + "\n");
+                    }
                 }
+                System.out.println(sb.toString());
             }
 
             case ALL -> {
                 List<GraphicObject> allObjects= objectManager.getAllObjects();
                 StringBuilder sb= new StringBuilder();
                 sb.append("Elenco di tutti gli oggetti:\n");
-                for( GraphicObject go : allObjects ){
-                    sb.append(objectManager.getKey(go) + " in posizione:" + go.getPosition().toString());
+                if(allObjects.isEmpty()){
+                    sb.append(" VUOTO");
+                }else {
+                    for (GraphicObject go : allObjects) {
+                        sb.append(" " + objectManager.getKeyByValue(go) + " in posizione:" + stampaPosizione(go) + "\n");
+                    }
                 }
+                System.out.println(sb.toString());
             }
 
             case GROUPS -> {
@@ -84,6 +108,11 @@ public class ListCommand implements Command{
 
 
         }
+    }
+
+    private String stampaPosizione(GraphicObject go) {
+        return "(" + String.format("%.2f",go.getPosition().getX()) + ";" + String.format("%.2f",go.getPosition().getY())
+                + ")";
     }
 
 
